37fc1729ff6cab4e60870aad6e1ee90f5481777e
Ndiforamang Fusi <fusisoft@gmail.com>
2022-10-06 21:30:46 -0500

    Added checked on students

--- text.java
+++ text.java
@@ -2,3 +2,5 @@ Git is a great tool to master
 Keep up the good work
 I hope you are understanding the importance of jenkins in a cd/cd process
 Please, let me know
+I hope you enjoyed the class today
+

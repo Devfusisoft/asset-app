3da08bf60f0454b835309b102f5901ff584ba4b4
Ndiforamang Fusi <fusisoft@gmail.com>
2022-10-08 11:24:52 -0500

    Catch up class

--- text.java
+++ text.java
@@ -3,4 +3,5 @@ Keep up the good work
 I hope you are understanding the importance of jenkins in a cd/cd process
 Please, let me know
 I hope you enjoyed the class today
-
+Catch up class today
+I hope you are loving it guys
